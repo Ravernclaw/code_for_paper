68f29773e22125ba600a674ccceec3ecafe2d6a9
Ravernclaw <90256414+Ravernclaw@users.noreply.github.com>
2023-03-25 11:58:17 +0800

    Delete main.m

--- main.m
+++
@@ -1,5 +0,0 @@
-load './train_data_smote.mat';
-load './test_1_data_smote.mat';
-load './test_2_data_smote.mat';
-sv_path='./result';
-demo_bls(traindata,label,testdata1,flag1,testdata2,flag2,sv_path);
